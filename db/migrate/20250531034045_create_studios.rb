459e87595debbeef175261df70407f40d107653d
Don Bartolome <donbartolome@gmail.com>
2025-05-30 21:03:38 -0700

    migrations

--- db/migrate/20250531034045_create_studios.rb
+++ db/migrate/20250531034045_create_studios.rb
@@ -1,12 +1,12 @@
 class CreateStudios < ActiveRecord::Migration[8.0]
   def change
     create_table :studios do |t|
-      t.string :name
-      t.string :address_line1
+      t.string :name, null: false, index: { unique: true }
+      t.string :address_line1, null: false
       t.string :address_line2
-      t.string :city
-      t.string :state
-      t.string :zip_code
+      t.string :city, null: false
+      t.string :state, null: false
+      t.string :zip_code, null: false
 
       t.timestamps
     end
