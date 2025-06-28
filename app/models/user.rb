4812e3f7b2d2c1ba06319047ade2e04d1917c00d
Don Bartolome <donbartolome@gmail.com>
2025-06-27 17:37:20 -0700

    unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   validates :email_address, presence: true
+  validates :password_digest, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,14 +1,28 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  test "should not save user without email" do
-    user = User.new(password: "password")
-    assert_not user.valid?
-    assert_includes user.errors[:email_address], "can't be blank"
+  def setup
+    @user = build(:user)
+  end
+
+  test "is valid with all required attributes" do
+    assert @user.valid?
+  end
+
+  test "is invalid without an email address" do
+    @user.email_address = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:email_address], "can't be blank"
+  end
+
+  test "is invalid without a password" do
+    @user.password_digest = nil
+    assert_not @user.valid?
+    assert_includes @user.errors[:password_digest], "can't be blank"
   end
 
   test "should authenticate with valid password" do
-    user = create(:user, password: "password")
-    assert user.authenticate("password")
+    # user = create(:user, password: "password")
+    assert @user.authenticate("password")
   end
 end
