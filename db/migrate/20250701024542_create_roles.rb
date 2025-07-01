d6b8fa2fb076611ab0a39cf55e36f4b4b317d397
Don Bartolome <donbartolome@gmail.com>
2025-06-30 22:30:47 -0700

    WIP

--- db/migrate/20250701024542_create_roles.rb
+++ db/migrate/20250701024542_create_roles.rb
@@ -1,10 +1,11 @@
 class CreateRoles < ActiveRecord::Migration[8.0]
   def change
     create_table :roles do |t|
-      t.string :name
-      t.text :allowed_scope_types
+      t.string :name, null: false
+      t.string :allowed_scope_types, array: true
 
       t.timestamps
     end
+    add_index :roles, :name, unique: true
   end
 end
