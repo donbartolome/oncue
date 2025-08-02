c944fb2d524c3b792511fa96aa09f9be64b5f14c
Don Bartolome <donbartolome@gmail.com>
2025-08-01 17:45:13 -0700

    migrations

--- db/migrate/20250701213510_drop_studios_table.rb
+++
@@ -1,9 +0,0 @@
-class DropStudiosTable < ActiveRecord::Migration[8.0]
-  def up
-    drop_table :studios
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration, "This migration cannot be reverted because it destroys data."
-  end
-end
