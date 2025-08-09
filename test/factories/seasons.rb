da04058be1d22eddefb9f9fe5764ac146da1336d
Don Bartolome <donbartolome@gmail.com>
2025-08-08 17:45:48 -0700

    Factory

--- test/factories/seasons.rb
+++ test/factories/seasons.rb
@@ -1,8 +1,33 @@
 FactoryBot.define do
   factory :season do
-    name { "MyString" }
-    start_year { 1 }
-    end_year { 1 }
-    studio { nil }
+    sequence(:name) { |n| "Season #{n}" }
+
+
+    start_year { Date.current.year }
+    end_year   { start_year + 1 }
+
+    studio
+
+    trait :past do
+      start_year { Date.current.year - 1 }
+      end_year   { start_year + 1 }
+    end
+
+    trait :future do
+      start_year { Date.current.year + 1 }
+      end_year   { start_year + 1 }
+    end
+
+    # Allows setting a specific year for both start and end
+    transient do
+      year { nil }
+    end
+
+    after(:build) do |season, evaluator|
+      if evaluator.year
+        season.start_year = evaluator.year
+        season.end_year   = evaluator.year
+      end
+    end
   end
 end
