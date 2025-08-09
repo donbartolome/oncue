30b74a2497123645be6017eca87ed2772fb0c115
Don Bartolome <donbartolome@gmail.com>
2025-08-08 17:30:37 -0700

    shallow route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :seasons
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -21,6 +20,8 @@ Rails.application.routes.draw do
       get "new_dancer"
       post "add_dancer"
     end
+
+    resources :seasons, shallow: true
   end
 
   # Defines the root path route ("/")
