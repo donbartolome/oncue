3a41879ee0f2182b513c2775f76522a57f74d767
Don Bartolome <donbartolome@gmail.com>
2025-08-08 17:28:42 -0700

    refine Season

--- db/migrate/20250809002633_create_seasons.rb
+++ db/migrate/20250809002633_create_seasons.rb
@@ -1,9 +1,9 @@
 class CreateSeasons < ActiveRecord::Migration[8.0]
   def change
     create_table :seasons do |t|
-      t.string :name
-      t.integer :start_year
-      t.integer :end_year
+      t.string :name, null: false
+      t.integer :start_year, null: false
+      t.integer :end_year, null: false
       t.references :studio, null: false, foreign_key: true
 
       t.timestamps
