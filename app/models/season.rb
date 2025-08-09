dfd466c65370b131c0a9cf7d64cd762ba8f2af5c
Don Bartolome <donbartolome@gmail.com>
2025-08-08 17:17:49 -0700

    Revert "Add seasons"
    This reverts commit 91a8dcf612fbdc2fea266da1e2906ca8577be7d0.

--- app/models/season.rb
+++
@@ -1,9 +0,0 @@
-class Season < ApplicationRecord
-  belongs_to :studio
-
-  validates :name, :start_year, :end_year, presence: true
-
-  def year
-    start_year == end_year ? start_year.to_s : "#{start_year} - #{end_year}"
-  end
-end
--- test/factories/seasons.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :season do
-    name { "MyString" }
-    start_year { 1 }
-    end_year { 1 }
-    studio { nil }
-  end
-end
