9202a3b432d9cb24b5db27b12b419492faee2be4
Don Bartolome <donbartolome@gmail.com>
2025-08-19 20:09:23 -0700

    seasons_controller_test

--- test/controllers/seasons_controller_test.rb
+++ test/controllers/seasons_controller_test.rb
@@ -2,47 +2,90 @@ require "test_helper"
 
 class SeasonsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @season = seasons(:one)
+    @user = create(:user)
+    @studio = create(:studio)
+    @season = create(:season, studio: @studio)
   end
 
-  test "should get index" do
-    get seasons_url
+  # index
+  test "renders index" do
+    get studio_seasons_url(@studio)
     assert_response :success
   end
 
-  test "should get new" do
-    get new_season_url
+  # new
+  test "renders new season form" do
+    get new_studio_season_url(@studio)
     assert_response :success
   end
 
-  test "should create season" do
+  # create
+  test "creates season" do
     assert_difference("Season.count") do
-      post seasons_url, params: { season: { end_year: @season.end_year, name: @season.name, start_year: @season.start_year, studio_id: @season.studio_id } }
+      post studio_seasons_url(@studio), params: { season: attributes_for(:season) }
     end
-
     assert_redirected_to season_url(Season.last)
   end
 
-  test "should show season" do
+  test "does not create season with invalid params" do
+    assert_no_difference("Season.count") do
+      post studio_seasons_url(@studio), params: { season: { name: "" } }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  # show
+  test "renders season" do
     get season_url(@season)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "returns 404 for show with invalid id" do
+    get season_url(-1)
+    assert_response :not_found
+  end
+
+  # edit
+  test "renders edit season form" do
     get edit_season_url(@season)
     assert_response :success
   end
 
-  test "should update season" do
-    patch season_url(@season), params: { season: { end_year: @season.end_year, name: @season.name, start_year: @season.start_year, studio_id: @season.studio_id } }
-    assert_redirected_to season_url(@season)
+  test "returns 404 for edit with invalid id" do
+    get edit_season_url(-1)
+    assert_response :not_found
   end
 
-  test "should destroy season" do
+  # update
+  test "updates season" do
+    patch season_url(@season), params: { season: { name: "Updated Season" } }
+    assert_redirected_to season_url(@season)
+    @season.reload
+    assert_equal "Updated Season", @season.name
+  end
+
+  test "does not update season with invalid params" do
+    patch season_url(@season), params: { season: { name: "" } }
+    assert_response :unprocessable_entity
+    @season.reload
+    assert_not_equal "", @season.name
+  end
+
+  test "returns 404 for update with invalid id" do
+    patch season_url(-1), params: { season: { name: "Doesn't Matter" } }
+    assert_response :not_found
+  end
+
+  # destroy
+  test "destroys season" do
     assert_difference("Season.count", -1) do
       delete season_url(@season)
     end
-
     assert_redirected_to seasons_url
+  end
+
+  test "returns 404 for destroy with invalid id" do
+    delete season_url(-1)
+    assert_response :not_found
   end
 end
